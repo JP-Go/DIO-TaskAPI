9c2a7934899581ee405ea0a33828d0b4922baa14
João Pedro <jpedro1580@gmail.com>
2023-10-26 01:05:34 -0300

    Configure db connection

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Organizer.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Pass your data source to the EF provider:
+builder.Services.AddDbContext<TaskContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
+);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
